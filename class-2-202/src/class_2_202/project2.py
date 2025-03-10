6b6f4c425e51df90a9425abcd659255fc98d3ac0
aqsashah <aqsanor665@gmail.com>
2025-03-10 22:22:22 +0500

    .

---
+++ class-2-202/src/class_2_202/project2.py
@@ -0,0 +1,7 @@
+# from selenium import webdriver
+# import time
+# def main():
+#     driver = webdriver.Chrome()
+#     driver.get("https://www.youtube.com/@panaverse/streams")
+#     time.sleep(5)
+#     driver.close()
